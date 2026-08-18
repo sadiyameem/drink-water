e79c70bffa844dd5eb6435bb3d6e48447a9a94a0
Sadiya <160082120+sadiyameem@users.noreply.github.com>
2026-08-18 16:34:35 -0400

    I added my new feature

--- script.js
+++ script.js
@@ -49,3 +49,23 @@ function updateBigCup() {
         liters.textContent = `${2 - (250 * fullCups) / 1000}L`;
     }
 }
+
+let btn = document.getElementById('btn');
+let output = document.getElementById('output');
+let quotes = [
+    'It forms saliva and mucus',
+    'It boost skin health',
+    'The digestive system depends on it',
+    'It helps maintain blood pressure',
+    'It flushes body waste',
+    'It prevents kidney damage',
+    'It reduces the chance of a hangover',
+    'The airways need it',
+    'It regulates body temperature',
+    'It delivers oxygen throughout the body'
+];
+
+btn.addEventListener('click', function() {
+    var randomQuote = quotes[Math.floor(Math.random() * quotes.length)]
+    output.innerHTML = randomQuote;
+})
